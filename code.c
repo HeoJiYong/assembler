95d988570e360253345cd40f8b0819a46cffb74f
HeoJiYong <hjy7547@naver.com>
2018-10-29 15:12:12 +0900

    Main Code add Sum Func_ HeoJiYong

--- code.c
+++ code.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+
+int sum(int x,int y)
+{
+return x+y;
+}
+
+
+
 int main()
 {
 int a =5;
-printf("%d, My First Code",a);
+printf("%d, My First Code\n",a);
+printf("sum = %d",sum(4,5));
 return 0;
 }
+
