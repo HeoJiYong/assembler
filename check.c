54214d8b7354c62880e5b6feb336f22ee882ac59
HeoJiYong <hjy7547@naver.com>
2018-11-14 01:07:52 +0900

    Finally This file is done. Bug Fixed

--- check.c
+++ check.c
@@ -5,7 +5,7 @@ int is_Reg(char *target)
 {
 	char Reg[8][6] = {"%eax","%ecx","%edx","%ebx","%esi","%edi","%esp","%dbp"};
 	int i=0;
-	for(int i=0;i<8;i++)
+	for(i=0;i<8;i++)
 	{
 		if(strcmp(target,Reg[i])==0)
 			return 1;
